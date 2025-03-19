89e5726b386402c499649e0b10e5db41b3bcc57d
eraymond606 <e.raymond6466@gmail.com>
2025-03-19 14:21:43 -0400

    Update App.js
    Changes to header and home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/Header';
+// import Header from './components/Header';
 import Home from './components/Home';
 import Configure from './components/ConfigOptions';
 import Orders from './components/Orders';
@@ -12,15 +12,15 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Header />
-        <nav>
+        {/* <Header /> */}
+        {/* <nav>
           <Link to="/">Home</Link> | <Link to="/ConfigOptions">Configure</Link>{' '}
           | <Link to="/orders">Orders</Link> | <Link to="/admin">Admin</Link>
-        </nav>
+        </nav> */}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Admin />} />
           <Route path="/ConfigOptions" element={<Configure />} />
-          <Route path="/orders" element={<Orders />} />
+          {/* <Route path="/orders" element={<Orders />} /> */}
           <Route path="/admin" element={<Admin />} />
           <Route path="/EmployeeConfig" element={<EmployeeConfig />} />
         </Routes>
@@ -30,3 +30,4 @@ function App() {
 }
 
 export default App;
+
